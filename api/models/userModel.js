8bd1adb3419d27bc6192945a70349e42242d43a5
Siufi04 <42659452+Siufi04@users.noreply.github.com>
2018-08-23 21:42:01 -0300

    Update userModel.js

--- api/models/userModel.js
+++ api/models/userModel.js
@@ -3,24 +3,61 @@ const mongoose = require("mongoose");
 
 const userSchema = mongoose.Schema(
   {
-    name: {
-    type: String,
-    required: true
-    },
-    lastname: {
-    type: String,
-    required: true
-    },
+    name: String,
+    lastname: String,
     email: {
       type: String,
-      required: true,
       unique: true
     },
-    password: {
-    type: String,
-    required: true
-    },
-    { timestamps: true }
+    password: String
+  },
+  { timestamps: true }
 );
 
 module.exports = mongoose.model("user", userSchema);
+
+const materiasSchema = mongoose.Schema(
+  {
+	nome_materia:{
+      type: String,
+	  required: true
+	}
+	
+	nome_prof:{
+	  type: String,
+	  required: true
+	}
+	
+	
+	{
+	semana: "Dias da semana",
+	dias:
+	[
+	{name: "Segunda",
+    type: Boolean},
+	
+   {name: "Terça",
+    type: Boolean,}
+	
+   {name: "Quarta",
+    type: Boolean,}
+	
+   {name: "Quinta",
+    type: Boolean,}
+	
+   {name: "Sexta",
+    type: Boolean,}
+	
+   {name: "Sabado",
+    type: Boolean,}
+	
+   {name: "Domingo",
+    type: Boolean,}
+ 
+  ]
+
+  },
+  {timestamps: true}
+ );
+
+}
